6ca43310b1ac84bb5c8e7d9d48d3722ee3cb8319
Sanjay Prajapati <43892590+sanjay270899@users.noreply.github.com>
2021-04-01 19:57:53 +0530

    Improved Footer (#7)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,32 +5,37 @@ import { FaLink, FaDiscord, FaLinkedin, FaFacebookSquare, FaInstagramSquare, FaG
 function Footer() {
     return (
         <div className={styles.Footer}>
-            <div>
-                <Row link="https://www.devsnest.in">
-                    <FaLink size={35} />
-                </Row>
-                <Row link="https://discord.gg/rGQCswxcx6">
-                    <FaDiscord size={35} />
-                </Row>
-                <Row link="https://www.linkedin.com/company/devsnest/">
-                    <FaLinkedin size={35} />
-                </Row>
-                <Row link="https://www.facebook.com/devsnest/">
-                    <FaFacebookSquare size={35} />
-                </Row>
-                <Row link="https://www.instagram.com/devsnest.in/">
-                    <FaInstagramSquare size={35} />
-                </Row>
+            <div className={styles.text}>
+                <p>We aim to make young India financially self-sustainable by providing equal opportunity for all.</p>
+                <div>
+                    <a className={styles.source_code} href="https://github.com/devs-nest/devsnest-teams" target="_blank" rel="noopener noreferrer">
+                        <FaGithub />&nbsp;GitHub
+                    </a>
+                    <a href="https://drive.google.com/file/d/17TKzId-5qlRjKVLX30W1Brn_L70rcMN1/view" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
+                </div>
             </div>
-
-            <div className={styles.email}>
-                support@devsnest.in
-            </div>
-
-            <div className={styles.source_code}>
-                <a href="https://github.com/devs-nest/devsnest-teams" target="_blank" rel="noopener noreferrer">
-                    <FaGithub />&nbsp;GitHub
-                </a>
+            <div className={styles.space}></div>
+            <div className={styles.connect}>
+                <div className={styles.email}>
+                    Contact Us: <a href="mailto: support@devsnest.in">support@devsnest.in</a>
+                </div>
+                <div>
+                    <Row link="https://www.devsnest.in">
+                        <FaLink size={25} color="darkblue" />
+                    </Row>
+                    <Row link="https://discord.gg/rGQCswxcx6">
+                        <FaDiscord size={25} color="darkblue" />
+                    </Row>
+                    <Row link="https://www.linkedin.com/company/devsnest/">
+                        <FaLinkedin size={25} color="darkblue" />
+                    </Row>
+                    <Row link="https://www.facebook.com/devsnest/">
+                        <FaFacebookSquare size={25} color="darkblue" />
+                    </Row>
+                    <Row link="https://www.instagram.com/devsnest.in/">
+                        <FaInstagramSquare size={25} color="darkblue" />
+                    </Row>
+                </div>
             </div>
         </div>
     )
